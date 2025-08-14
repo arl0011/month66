a6163cdc5bf006341dfcd26595d47cb9cf824aaa
Arlen <arlenbajgazakov@gmail.com>
2025-08-14 17:50:04 +0600

    hw4

--- product/serializers.py
+++ product/serializers.py
@@ -9,11 +9,31 @@ class CategorySerializer(serializers.ModelSerializer):
 
     def get_products_count(self, obj):
         return obj.products.count()
+    
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Название категории должно быть не короче 3 символов.")
+        
+        if Category.objects.filter(name__iexact=value).exists():
+            raise serializers.ValidationError("Категория с таким именем уже существует.")
+
+        return value
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+
+
+    def validate_text(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Текст отзыва не может быть пустым.")
+        return value
+
+    def validate_stars(self, value):
+        if not (1 <= value <= 5):
+            raise serializers.ValidationError("Оценка (stars) должна быть от 1 до 5.")
+        return value    
 
 class ProductSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
@@ -22,6 +42,23 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'title', 'description', 'price', 'category', 'reviews','rating']
+
+
+    def validate_title(self, value):
+        if len(value.strip()) < 3:
+            raise serializers.ValidationError("Название товара должно быть не короче 3 символов.")
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Цена должна быть больше нуля.")
+        return value
+
+    def get_reviews(self, obj):
+        from .serializers import ReviewSerializer  
+        return ReviewSerializer(obj.reviews.all(), many=True).data
+
+    
 
 
     def get_rating(self, obj):
